8f7bfbe1560f6242866ed8208f14bd0f89f3cf1d
peterson <peterson.machado064@gmail.com>
2023-05-30 00:41:31 -0300

    AA

--- main.js
+++ main.js
@@ -10,23 +10,12 @@ new Vue({
                 description: 'Olá, sou um desenvolvedor Full Stack, especializado em Front-end e Desktop. Tenho experiência em tecnologias como Java, Spring Boot, Hibernate, Mysql e Vue, e trabalho com dedicação para produzir soluções inovadoras e eficientes para os clientes.'
             },
             2: {
-                name: 'Valter Junior',
+                name: 'José Francisco',
                 description: 'Tudo bem? Eu sou especialista em desenvolvimento back-end. Precisar de uma solução de software com PHP, Laravel, Node, Python, JS e C. Ofereço soluções personalizadas e eficientes para atender às suas necessidades.'
             },
             3: {
-                name: 'Antonio Henrique',
+                name: 'Ramon',
                 description: 'Seja bem-vindo! Ofereço serviços de alta qualidade na área de design e desenvolvimento front-end, com ampla experiência com HTML/CSS. Estou pronto para atender às suas necessidades e garantir um resultado incrível.'
-            }
-        },
-        tecnologia: {
-            1:{
-                icones: '<i class="fa-brands fa-java"></i> <i class="fa fa-database"></i> <i class="fa-brands fa-bootstrap"></i> <i class="fa-brands fa-vuejs"></i>',
-            },
-            2:{
-                icones: '<i class="fa-brands fa-php"></i> <i class="fa-brands fa-laravel"></i> <i class="fa-brands fa-python"></i> <i class="fa-brands fa-js"></i>',
-            },
-            3:{
-                icones: '<i class="fa-brands fa-html5"></i> <i class="fa-brands fa-css3-alt"></i> <i class="fa-brands fa-sass"></i>',
             }
         }
     },
@@ -34,7 +23,6 @@ new Vue({
         setPerfil(value) {
             this.name = this.perfil[value].name;
             this.description = this.perfil[value].description;
-            this.icones = this.tecnologia[value].icones;
 
             const duration = 200;
             const descricao = document.querySelector('.descricao');
