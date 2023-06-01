4cc92225baa99591d2f3b580e493089bb58695eb
peterson <peterson.machado064@gmail.com>
2023-06-01 02:19:29 -0300

    Sitezinho

--- main.js
+++ main.js
@@ -6,16 +6,24 @@ new Vue({
         icones: '',
         perfil: {
             1: {
-                name: 'José Peterson',
-                description: 'Olá, sou um desenvolvedor Full Stack, especializado em Front-end e Desktop. Tenho experiência em tecnologias como Java, Spring Boot, Hibernate, Mysql e Vue, e trabalho com dedicação para produzir soluções inovadoras e eficientes para os clientes.'
+                name: 'José Francisco',
+                description: 'Especialista em Produtos: Sou especialista em produtos de hardware e estou atualizado com as últimas tendências e tecnologias do setor. Eu forneço informações técnicas e consultoria aos clientes, ajuda na seleção dos melhores produtos para atender às necessidades do cliente e mantém-se atualizado sobre as especificações e características dos produtos disponíveis.'
             },
             2: {
-                name: 'José Francisco',
-                description: 'Tudo bem? Eu sou especialista em desenvolvimento back-end. Precisar de uma solução de software com PHP, Laravel, Node, Python, JS e C. Ofereço soluções personalizadas e eficientes para atender às suas necessidades.'
+                name: 'José Peterson',
+                description: 'Gerente de Vendas: Sou responsável em liderar a equipe de vendas, definir metas, desenvolver estratégias para impulsionar as vendas e garantir um excelente atendimento ao cliente. Eu supervisiono o desempenho da equipe, analiso os relatórios de vendas, estabeleço parcerias com fornecedores e busca oportunidades de crescimento no mercado.'
             },
             3: {
+                name: 'Rafael',
+                description: 'CEO da empresa: Eu estou encarregado de definir a visão estratégica e o direcionamento geral da organização. Eu tomo as decisões de alto nível, estabeleço metas e objetivos, desenvolvo parcerias estratégicas, gerencio as finanças da empresa e sou responsável por liderar a equipe executiva.'
+            },
+            4: {
                 name: 'Ramon',
-                description: 'Seja bem-vindo! Ofereço serviços de alta qualidade na área de design e desenvolvimento front-end, com ampla experiência com HTML/CSS. Estou pronto para atender às suas necessidades e garantir um resultado incrível.'
+                description: 'Logística e Estoquista: Sou responsável pela gestão do estoque, recebimento de mercadorias, controle de inventário e organização do armazém. Eu também cuido da embalagem e envio dos produtos aos clientes, garantindo que as entregas sejam feitas de forma eficiente e dentro do prazo estabelecido.'
+            },
+            5: {
+                name: 'Macio',
+                description: 'Marketing Digital: Sou responsável pela estratégia de marketing digital da empresa. Eu gerencio as mídias sociais, crio campanhas de publicidade online, otimizo a presença online da empresa e monitora as métricas de desempenho para garantir a eficácia das estratégias de marketing.'
             }
         }
     },
